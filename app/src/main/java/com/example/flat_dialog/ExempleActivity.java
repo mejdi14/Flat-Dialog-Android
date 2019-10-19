39e159388e80804c62c4b5679306e03cbfa5de17
unknown <mejdiclubiste@gmail.com>
2019-10-19 22:53:50 +0100

    save changes

--- app/src/main/java/com/example/flat_dialog/ExempleActivity.java
+++ app/src/main/java/com/example/flat_dialog/ExempleActivity.java
@@ -2,6 +2,9 @@ package com.example.flat_dialog;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,18 +12,202 @@ import com.example.flatdialoglibrary.dialog.FlatDialog;
 
 public class ExempleActivity extends AppCompatActivity {
 
+    Button loginDialog;
+    Button colorfulDialog;
+    Button largeDialog;
+    Button iconDialog;
+    Button optionDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new FlatDialog(ExempleActivity.this).setFirstButtonColor(Color.parseColor("#000000"))
-                .setFirstButtonTextColor(Color.parseColor("#FFFFFF"))
-                .setFirstButtonText("hello baby")
-                .setBackgroundColor(Color.parseColor("#f7be16"))
+        loginDialog = findViewById(R.id.login_dialog);
+        colorfulDialog = findViewById(R.id.colorful_dialog);
+        largeDialog = findViewById(R.id.large_dialog);
+        iconDialog = findViewById(R.id.icon_dialog);
+        optionDialog = findViewById(R.id.option_dialog);
+
+
+        loginDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showLoginDialog();
+            }
+        });
+
+        colorfulDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showColorfulDialog();
+            }
+        });
+
+        largeDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showLargeInputDialog();
+            }
+        });
+
+        iconDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showIconDialog();
+            }
+        });
+
+        optionDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showOptionDialog();
+            }
+        });
+
+    }
+
+
+    private void showOptionDialog() {
+        final FlatDialog flatDialog = new FlatDialog(ExempleActivity.this);
+        flatDialog.setTitle("Option dialog")
+                .setTitleColor(Color.parseColor("#000000"))
+                .setBackgroundColor(Color.parseColor("#f9fce1"))
+                .setFirstButtonColor(Color.parseColor("#d3f6f3"))
+                .setFirstButtonTextColor(Color.parseColor("#000000"))
+                .setFirstButtonText("ADD")
+                .setSecondButtonColor(Color.parseColor("#fee9b2"))
+                .setSecondButtonTextColor(Color.parseColor("#000000"))
+                .setSecondButtonText("UPDATE")
+                .setThirdButtonColor(Color.parseColor("#fbd1b7"))
+                .setThirdButtonTextColor(Color.parseColor("#000000"))
+                .setThirdButtonText("DELETE")
+                .withFirstButtonListner(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        flatDialog.dismiss();
+                    }
+                })
+                .withSecondButtonListner(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        flatDialog.dismiss();
+                    }
+                })
+                .withThirdButtonListner(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        flatDialog.dismiss();
+                    }
+                })
                 .show();
+    }
 
+    private void showIconDialog() {
+        final FlatDialog flatDialog = new FlatDialog(ExempleActivity.this);
+        flatDialog.setIcon(R.drawable.crying)
+                .setTitle("Somthing went wrong !")
+                .setTitleColor(Color.parseColor("#000000"))
+                .setSubtitle("choose an action")
+                .setSubtitleColor(Color.parseColor("#000000"))
+                .setBackgroundColor(Color.parseColor("#a26ea1"))
+                .setFirstButtonColor(Color.parseColor("#f18a9b"))
+                .setFirstButtonTextColor(Color.parseColor("#000000"))
+                .setFirstButtonText("Try again")
+                .setSecondButtonColor(Color.parseColor("#ffff9d"))
+                .setSecondButtonTextColor(Color.parseColor("#000000"))
+                .setSecondButtonText("Send rapport")
+                .withFirstButtonListner(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        flatDialog.dismiss();
+                    }
+                })
+                .withSecondButtonListner(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        flatDialog.dismiss();
+                    }
+                })
+                .show();
+    }
 
+    private void showLargeInputDialog() {
+        final FlatDialog flatDialog = new FlatDialog(ExempleActivity.this);
+        flatDialog.setTitle("Send a message")
+                .setTitleColor(Color.parseColor("#000000"))
+                .setBackgroundColor(Color.parseColor("#f5f0e3"))
+                .setLargeTextFieldHint("write your text here ...")
+                .setLargeTextFieldHintColor(Color.parseColor("#000000"))
+                .setLargeTextFieldBorderColor(Color.parseColor("#000000"))
+                .setLargeTextFieldTextColor(Color.parseColor("#000000"))
+                .setFirstButtonColor(Color.parseColor("#fda77f"))
+                .setFirstButtonTextColor(Color.parseColor("#000000"))
+                .setFirstButtonText("Done")
+                .withFirstButtonListner(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        flatDialog.dismiss();
+                    }
+                })
+                .show();
+    }
 
+    private void showColorfulDialog() {
+        final FlatDialog flatDialog = new FlatDialog(ExempleActivity.this);
+        flatDialog.setBackgroundColor(Color.parseColor("#1a2849"))
+                .setTitle("Profile")
+                .setFirstTextFieldHint("write here anything !")
+                .setFirstButtonColor(Color.parseColor("#e3c878"))
+                .setFirstButtonTextColor(Color.parseColor("#FFFFFF"))
+                .setFirstButtonText("ADD")
+                .setSecondButtonColor(Color.parseColor("#ed9a73"))
+                .setSecondButtonTextColor(Color.parseColor("#FFFFFF"))
+                .setSecondButtonText("UPDATE")
+                .setThirdButtonColor(Color.parseColor("#e688a1"))
+                .setThirdButtonTextColor(Color.parseColor("#FFFFFF"))
+                .setThirdButtonText("DELETE")
+                .withFirstButtonListner(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        flatDialog.dismiss();
+                    }
+                })
+                .withSecondButtonListner(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        flatDialog.dismiss();
+                    }
+                })
+                .withThirdButtonListner(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        flatDialog.dismiss();
+                    }
+                })
+                .show();
+    }
+
+    private void showLoginDialog() {
+        final FlatDialog flatDialog = new FlatDialog(ExempleActivity.this);
+        flatDialog.setTitle("Login")
+                .setSubtitle("write your profile info here")
+                .setFirstTextFieldHint("email")
+                .setSecondTextFieldHint("password")
+                .setFirstButtonText("CONNECT")
+                .setSecondButtonText("CANCEL")
+                .withFirstButtonListner(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(ExempleActivity.this, flatDialog.getFirstTextField(), Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .withSecondButtonListner(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        flatDialog.dismiss();
+                    }
+                })
+                .show();
     }
 }
--- flatdialoglibrary/src/main/java/com/example/flatdialoglibrary/dialog/FlatDialog.java
+++ flatdialoglibrary/src/main/java/com/example/flatdialoglibrary/dialog/FlatDialog.java
@@ -2,6 +2,7 @@ package com.example.flatdialoglibrary.dialog;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
@@ -17,26 +18,27 @@ import com.example.flatdialoglibrary.R;
 
 
 public class FlatDialog extends Dialog {
-    Context context;
-    TextView title;
+    private Context context;
 
-    TextView subtitle;
+    private TextView title;
 
-    TextView first_button;
+    private TextView subtitle;
 
-    TextView second_button;
+    private TextView first_button;
 
-    TextView third_button;
+    private TextView second_button;
 
-    EditText first_edit_text;
+    private TextView third_button;
 
-    EditText second_edit_text;
+    private EditText first_edit_text;
 
-    EditText large_edit_text;
+    private EditText second_edit_text;
 
-    ImageView icon;
+    private EditText large_edit_text;
 
-    LinearLayout main_frame;
+    private ImageView icon;
+
+    private LinearLayout main_frame;
 
     public FlatDialog(Context context) {
         super(context);
@@ -53,9 +55,33 @@ public class FlatDialog extends Dialog {
         large_edit_text=findViewById(R.id.large_edit_text);
         icon=findViewById(R.id.icon);
         main_frame=findViewById(R.id.main_frame);
-        this.getWindow().setBackgroundDrawableResource(android.R.color.white);
+        this.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
+        initDefaultCase();
         this.setCancelable(false);
 
+
+
+    }
+
+    private void initDefaultCase() {
+        setLargeTextFieldBorderColor(Color.parseColor("#FFFFFF"));
+        setFirstTextFieldBorderColor(Color.parseColor("#FFFFFF"));
+        setSecondTextFieldBorderColor(Color.parseColor("#FFFFFF"));
+        setTitleColor(Color.parseColor("#FFFFFF"));
+        setSubtitleColor(Color.parseColor("#FFFFFF"));
+        setFirstButtonColor(Color.parseColor("#8A56AC"));
+        setSecondButtonColor(Color.parseColor("#D47FA6"));
+        setThirdButtonColor(Color.parseColor("#998FA2"));
+        setBackgroundColor(Color.parseColor("#241332"));
+        first_edit_text.setVisibility(View.GONE);
+        second_edit_text.setVisibility(View.GONE);
+        large_edit_text.setVisibility(View.GONE);
+        third_button.setVisibility(View.GONE);
+        first_button.setVisibility(View.GONE);
+        second_button.setVisibility(View.GONE);
+        title.setVisibility(View.GONE);
+        subtitle.setVisibility(View.GONE);
 
     }
 
@@ -75,15 +101,12 @@ public class FlatDialog extends Dialog {
         title.setText(titleText);
         return this;
     }
-
-   /* public FlatDialog isHasTitle(boolean hasTitle){
-        if (hasTitle) {
-            title.setVisibility(View.VISIBLE);
-        } else {
-            title.setVisibility(View.GONE);
-        }
+    public FlatDialog setTitleColor(int color) {
+        title.setVisibility(View.VISIBLE);
+        title.setTextColor(color);
         return this;
-    }*/
+    }
+
 
     public FlatDialog setSubtitle(String subtitleText) {
         subtitle.setVisibility(View.VISIBLE);
@@ -91,15 +114,11 @@ public class FlatDialog extends Dialog {
         return this;
     }
 
-  /*  public FlatDialog isHasSubtitle(boolean hasSubtitle){
-        if (hasSubtitle) {
-            subtitle.setVisibility(View.VISIBLE);
-        } else {
-            subtitle.setVisibility(View.GONE);
-        }
+    public FlatDialog setSubtitleColor(int color) {
+        subtitle.setVisibility(View.VISIBLE);
+        subtitle.setTextColor(color);
         return this;
-    }*/
-
+    }
     public String getFirstTextField() {
         return first_edit_text.getText().toString();
     }
@@ -140,82 +159,118 @@ public class FlatDialog extends Dialog {
     }
 
     public FlatDialog setFirstTextField(String firstText) {
+        first_edit_text.setVisibility(View.VISIBLE);
         first_edit_text.setText(firstText);
         return this;
     }
 
     public FlatDialog setSecondTextField(String secondText) {
+        second_edit_text.setVisibility(View.VISIBLE);
         second_edit_text.setText(secondText);
         return this;
     }
 
     public FlatDialog setLargeTextField(String secondText) {
+        large_edit_text.setVisibility(View.VISIBLE);
         large_edit_text.setText(secondText);
         return this;
     }
 
     public FlatDialog setFirstTextFieldHint(String firstText) {
+        first_edit_text.setVisibility(View.VISIBLE);
         first_edit_text.setHint(firstText);
         return this;
     }
 
     public FlatDialog setSecondTextFieldHint(String secondText) {
+        second_edit_text.setVisibility(View.VISIBLE);
         second_edit_text.setHint(secondText);
         return this;
     }
 
     public FlatDialog setLargeTextFieldHint(String secondText) {
+        large_edit_text.setVisibility(View.VISIBLE);
         large_edit_text.setHint(secondText);
         return this;
     }
 
 
     public FlatDialog setFirstTextFieldTextColor(int color) {
+        first_edit_text.setVisibility(View.VISIBLE);
         first_edit_text.setTextColor(color);
         return this;
     }
 
     public FlatDialog setSecondTextFieldTextColor(int color) {
+        second_edit_text.setVisibility(View.VISIBLE);
         second_edit_text.setTextColor(color);
         return this;
     }
 
     public FlatDialog setLargeTextFieldTextColor(int color) {
+        large_edit_text.setVisibility(View.VISIBLE);
         large_edit_text.setTextColor(color);
+        return this;
+    }
+    public FlatDialog setFirstTextFieldBorderColor(int color) {
+        first_edit_text.setVisibility(View.VISIBLE);
+        GradientDrawable drawable = (GradientDrawable)first_edit_text.getBackground();
+        drawable.setStroke(5, color);
+        return this;
+    }
+
+    public FlatDialog setSecondTextFieldBorderColor(int color) {
+        second_edit_text.setVisibility(View.VISIBLE);
+        GradientDrawable drawable = (GradientDrawable)second_edit_text.getBackground();
+        drawable.setStroke(5, color);
+        return this;
+    }
+
+    public FlatDialog setLargeTextFieldBorderColor(int color) {
+        large_edit_text.setVisibility(View.VISIBLE);
+        GradientDrawable drawable = (GradientDrawable)large_edit_text.getBackground();
+        drawable.setStroke(5, color);
         return this;
     }
 
     public FlatDialog setFirstTextFieldHintColor(int color) {
+        first_edit_text.setVisibility(View.VISIBLE);
         first_edit_text.setHintTextColor(color);
         return this;
     }
 
     public FlatDialog setSecondTextFieldHintColor(int color) {
+        second_edit_text.setVisibility(View.VISIBLE);
         second_edit_text.setHintTextColor(color);
         return this;
     }
 
     public FlatDialog setLargeTextFieldHintColor(int color) {
+        large_edit_text.setVisibility(View.VISIBLE);
         large_edit_text.setHintTextColor(color);
         return this;
     }
 
 
     public FlatDialog setFirstTextFieldInputType(int type) {
+        first_edit_text.setVisibility(View.VISIBLE);
         first_edit_text.setInputType(type);
         return this;
     }
     public FlatDialog setSecondTextFieldInputType(int type) {
+        second_edit_text.setVisibility(View.VISIBLE);
         second_edit_text.setInputType(type);
         return this;
     }
 
     public FlatDialog setLargeTextFieldInputType(int type) {
+        second_edit_text.setVisibility(View.VISIBLE);
         second_edit_text.setInputType(type);
         return this;
     }
 
     public FlatDialog setFirstButtonColor(int color) {
+        first_button.setVisibility(View.VISIBLE);
         Drawable background = first_button.getBackground();
         changingBackgroundColor(background, color);
         return this;
@@ -223,42 +278,50 @@ public class FlatDialog extends Dialog {
 
 
     public FlatDialog setSecondButtonColor(int color) {
+        second_button.setVisibility(View.VISIBLE);
         Drawable background = second_button.getBackground();
         changingBackgroundColor(background, color);
         return this;
     }
     public FlatDialog setThirdButtonColor(int color) {
+        third_button.setVisibility(View.VISIBLE);
         Drawable background = third_button.getBackground();
         changingBackgroundColor(background,color);
         return this;
     }
 
     public FlatDialog setFirstButtonTextColor(int color) {
+        first_button.setVisibility(View.VISIBLE);
         first_button.setTextColor(color);
         return this;
     }
 
 
     public FlatDialog setSecondButtonTextColor(int color) {
+        second_button.setVisibility(View.VISIBLE);
         second_button.setTextColor(color);
         return this;
     }
     public FlatDialog setThirdButtonTextColor(int color) {
+        third_button.setVisibility(View.VISIBLE);
         third_button.setTextColor(color);
         return this;
     }
 
     public FlatDialog setFirstButtonText(String text) {
+        first_button.setVisibility(View.VISIBLE);
         first_button.setText(text);
         return this;
     }
 
 
     public FlatDialog setSecondButtonText(String text) {
+        second_button.setVisibility(View.VISIBLE);
         second_button.setText(text);
         return this;
     }
     public FlatDialog setThirdButtonText(String text) {
+        third_button.setVisibility(View.VISIBLE);
         third_button.setText(text);
         return this;
     }
@@ -271,16 +334,19 @@ public class FlatDialog extends Dialog {
 
 
     public FlatDialog withFirstButtonListner(View.OnClickListener confirmListner){
+        first_button.setVisibility(View.VISIBLE);
         first_button.setOnClickListener(confirmListner);
         return this;
     }
 
     public FlatDialog withSecondButtonListner(View.OnClickListener cancelListner){
+        second_button.setVisibility(View.VISIBLE);
         second_button.setOnClickListener(cancelListner);
         return this;
     }
 
     public FlatDialog withThirdButtonListner(View.OnClickListener cancelListner){
+        third_button.setVisibility(View.VISIBLE);
         third_button.setOnClickListener(cancelListner);
         return this;
     }
